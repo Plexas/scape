8b4032718524afd532672c43ca9c2380baf441f0
DV-Nikkl <mail@nikkl.net>
2020-02-22 15:55:58 +0100

    Fix bug

--- src/Scaper.php
+++ src/Scaper.php
@@ -10,7 +10,7 @@ class Scaper
     const REGEX_USERNAME = "[^A-Za-z0-9_.]+";
     const REGEX_EMAIL = "[^A-Za-z0-9.@äöü\-_!&%~*+]+";
 
-    public function escape($string, $pattern = self::REGEX_DEFAULT)
+    public static function escape($string, $pattern = self::REGEX_DEFAULT)
     {
         return preg_filter($pattern, '', $string);
     }
